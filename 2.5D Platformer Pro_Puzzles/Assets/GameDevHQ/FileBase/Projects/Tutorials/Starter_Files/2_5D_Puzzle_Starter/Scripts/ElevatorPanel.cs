760262df55576022ebc42adcde9a96742665ebfc
Michiel Burger <michielburger0@gmail.com>
2020-08-15 09:51:00 +1200

    Call Button
    Change call button colour after OnTriggerStay

---
+++ 2.5D Platformer Pro_Puzzles/Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Puzzle_Starter/Scripts/ElevatorPanel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorPanel : MonoBehaviour
+{
+    [SerializeField]
+    private MeshRenderer _callButton;
+    
+    //check collision
+    //check player
+    //if e key is pressed
+    //turn light to blue
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                _callButton.material.color = Color.green;
+            }
+        }
+    }
+}

d60a0065312e105ed17e84cbc923cbe55331dcc2
Michiel Burger <michielburger0@gmail.com>
2020-08-19 10:00:49 +1200

    Elevator behaviour
    Elevator up and down movement implemented and jitter fixed.

---
+++ 2.5D Platformer Pro_Puzzles/Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Puzzle_Starter/Scripts/Elevator.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _pointA, _pointB;
+    [SerializeField]
+    private float _elevatorSpeed = 5.0f;
+    [SerializeField]
+    private bool _elevatorDown = false;
+
+    public void CallElevator()
+    {
+        //know position of the elevator
+
+        _elevatorDown = !_elevatorDown;
+        
+    }
+
+    void FixedUpdate()
+    {
+        //going down == true
+        //move.towards target position a
+        //else
+        //reverse
+
+        if (_elevatorDown == true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _pointB.position, _elevatorSpeed * Time.deltaTime);
+        }
+
+        else if(_elevatorDown == false)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _pointA.position, _elevatorSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            other.transform.parent = this.transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            other.transform.parent = null;
+        }
+    }
+}
--- 2.5D Platformer Pro_Puzzles/Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Puzzle_Starter/Scripts/ElevatorPanel.cs
+++ 2.5D Platformer Pro_Puzzles/Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Puzzle_Starter/Scripts/ElevatorPanel.cs
@@ -9,10 +9,25 @@ public class ElevatorPanel : MonoBehaviour
     [SerializeField]
     private int _requiredCoins = 8;
     
+    public bool elevatorCalled = false;
+
+    private Elevator _elevator;
+    private bool _elevatorCalled = false;
+
     //check collision
     //check player
     //if e key is pressed
     //turn light to blue
+
+    private void Start()
+    {
+        _elevator = GameObject.Find("Elevator").GetComponent<Elevator>();
+
+        if(_elevator == null)
+        {
+            Debug.LogError("Elevator is NULL");
+        }
+    }
 
     private void OnTriggerStay(Collider other)
     {
@@ -20,7 +35,18 @@ public class ElevatorPanel : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.E) && other.GetComponent<Player>().CointCount() >= _requiredCoins)
             {
-                _callButton.material.color = Color.green;
+
+                if(_elevatorCalled == true)
+                {
+                    _callButton.material.color = Color.red;
+                }
+                else if (_elevatorCalled == false)
+                {
+                    _callButton.material.color = Color.green;
+                    _elevatorCalled = true;
+                }
+                
+                _elevator.CallElevator();
             }
         }
     }

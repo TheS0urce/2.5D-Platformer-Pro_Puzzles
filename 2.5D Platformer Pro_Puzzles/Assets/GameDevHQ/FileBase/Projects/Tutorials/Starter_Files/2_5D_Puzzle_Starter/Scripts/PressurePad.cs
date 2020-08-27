3ab7a8a49f4bff64a541c5ba99d1a65c6ca6713a
Michiel Burger <michielburger0@gmail.com>
2020-08-27 13:52:04 +1200

    Pressure Pad
    My solution implemented

---
+++ 2.5D Platformer Pro_Puzzles/Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Puzzle_Starter/Scripts/PressurePad.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePad : MonoBehaviour
+{
+    [SerializeField]
+    private MeshRenderer _boxColour;
+
+    //detect moving box
+    //when close to centre
+    //disable box's rigidbody or set it to kinematic
+    //change colour of pressure pad to blue
+
+    private void OnTriggerStay(Collider other)
+    {
+        Rigidbody rb = other.GetComponent<Rigidbody>();
+
+        if (other.tag == "MovingBox")
+        {
+            if (other.transform.position.x < .4f)
+            {
+                rb.isKinematic = true;
+                _boxColour.material.color = Color.blue;
+            }
+        }
+    }
+}
